b61627db01fb7e89f0c165904c6ca8929a82dbfa
FrankieFigz <fjvannini@gmail.com>
2019-11-04 09:17:14 -0500

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+var mysqlEnv = {
+    user_name: process.env.USER,
+    password: process.env.PASSWORD,
+    host_name: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DATABASE
+};
+
+module.exports = {
+    mysqlEnv: mysqlEnv
+};
---
+++ db/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO burgers(burgername,date) VALUES ('cheeseburger');
